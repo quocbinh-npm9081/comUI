350a6b951b5aa8adaec8b6146a9c5e895706d06c
louis <quocbinhgithub@gmail.com>
2023-03-31 22:20:17 +0700

    add order component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CartComponent } from './cart/cart.component';
     HomeComponent,
     ProductsComponent,
     ProductDetailComponent,
-    CartComponent
+    CartComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order/order.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss'],
+})
+export class OrderComponent {
+  selectedPaymentMethodName = 'a';
+  selectedPaymentMethod = new FormControl('0');
+  ngOnInit(): void {
+    this.selectedPaymentMethod.valueChanges.subscribe((res: any) => {
+      if (res === '0') this.selectedPaymentMethodName = '';
+      else this.selectedPaymentMethodName = res.toString();
+    });
+  }
+}
